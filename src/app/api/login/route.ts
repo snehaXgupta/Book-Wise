640c6e30722b1ff8e034e2ae793b24b3a928ca37
Sneha Gupta <shubhigupta1078@gmail.com>
2025-07-25 21:13:57 +0530

    🥐

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@/generated/prisma';
+import { PrismaClient } from '@prisma/client';
 import { verifyPassword } from '@/lib/auth.ts';
 
 const prisma = new PrismaClient();
